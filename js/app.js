932b65737f8d7ca0c1d69eeb8b23bb787cab8697
RegularSvensson <elias.svensson.1992@gmail.com>
2015-03-26 14:36:48 +0100

    Remove old app.js code

--- js/app.js
+++ js/app.js
@@ -1,13 +1,3 @@
 $( document ).ready(function() {
-	$( "#cat1" ).click(function() {
-		var count = $("#count1").text();
-		count = parseInt(count) + 1;
-		$("#count1").text(count);
-	});
-
-	$( "#cat2" ).click(function() {
-		var count = $("#count2").text();
-		count = parseInt(count) + 1;
-		$("#count2").text(count);
-	});
+	
 });
