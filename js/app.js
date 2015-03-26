13e6c6dd0af9b0d3ef4fdd5b8265478974b4651c
RegularSvensson <elias.svensson.1992@gmail.com>
2015-03-26 18:56:05 +0100

    Add cat count to js

--- js/app.js
+++ js/app.js
@@ -12,12 +12,14 @@ $( document ).ready(function() {
 	/* ======= View ======= */
 	var catView = {
 		init: function() {
+			this.catCountElem = document.getElementById('cat-count');
 			this.catNameElem = document.getElementById('cat-name');
 			this.catImageElem = document.getElementById('cat-img');
 
 			this.render();
 		},
 		render: function() {
+			this.catCountElem.textContent = octopus.getCat().clickCount;
 			this.catNameElem.textContent = octopus.getCat().name;
 			this.catImageElem.src = octopus.getCat().imgSrc;
 		}
